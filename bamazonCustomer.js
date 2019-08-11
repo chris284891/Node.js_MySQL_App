22ffc3d2e1a651e53c841ca8ad1810a3d8429df8
chris284891 <chris284891@gmail.com>
2019-08-11 10:38:04 -0400

    changed host in mysql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,16 +3,17 @@ var inquirer = require("inquirer");
 var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306, 
+    host: "127.0.0.1",
+    port: 3306,
     user: "root",
     password: "password",
     database: "bamazon"
 });
+console.log(connection)
 
 connection.connect();
 
-var display = function() {
+var display = function () {
     connection.query("Select * FROM Products", function (err, res) {
         if (err) throw err;
         console.log("------------------------------");
@@ -21,18 +22,17 @@ var display = function() {
         console.log("------------------------------");
         console.log("Please Pick Your Product");
         console.log("------------------------------");
+        for (i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
     });
     var table = new Table({
         head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity'],
         colWidths: [10, 50, 50, 10, 10]
     });
-    for (i = 0; i < res.length; i++) {
-        table.push ([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
-    }
     console.log(table.toString());
     console.log("");
 };
-display();
 // console.log("---------------------")
 // console.log("ID | Products | Price")
 // console.log("---------------------")
@@ -43,18 +43,18 @@ display();
 //     })
 //     itemChoice();
 // }
-var shopping + function {
-    inquirer.prompt([{
-        name: "answer",
-        type: "input",
-        message: "What item would you like to buy? Please enter the corresponding Item ID number",
-        filter: Number
-    },
-    ])
-        .then(function (answer) {
-            var productID = answer;
-            productCheck(productID);
-        });
-    function productCheck(answers) {
-    }
-}
+// var shopping + function {
+//     inquirer.prompt({
+//         name: "answerToBuy",
+//         type: "input",
+//         message: "What item would you like to buy? Please enter the corresponding Item ID number",
+//         filter: Number
+//     },
+//     ).then(function (answer)) {
+//         var selection.answerToBuy;
+        
+//     }
+    
+    
+// }
+display();
